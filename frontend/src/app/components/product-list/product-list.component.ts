5127fe143ac4c47f5962e8249c7e8b7ba098c0b1
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-08-24 18:38:35 -0300

    Busca e detalhes

--- frontend/src/app/components/product-list/product-list.component.ts
+++ frontend/src/app/components/product-list/product-list.component.ts
@@ -6,31 +6,50 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: Product[];
   categoryId: number;
-  
-  constructor(private productService: ProductService, 
-              private route: ActivatedRoute) { }
+  searchMode: boolean;
+
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(() => { 
+    this.route.paramMap.subscribe(() => {
       this.listProducts();
-    })    
+    });
   }
 
   listProducts() {
-    const hasCategoryIdParam: boolean = this.route.snapshot.paramMap.has('id');
-    this.categoryId = hasCategoryIdParam ? +this.route.snapshot.paramMap.get('id') : 1;
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
 
-    this.productService.getProductList(this.categoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
   }
 
+  handleSearchProducts() {
+    const keyword: string = this.route.snapshot.paramMap.get('keyword');
+
+    this.productService.searchProducts(keyword).subscribe((data) => {
+      this.products = data;
+    });
+  }
+
+  handleListProducts() {
+    const hasCategoryIdParam: boolean = this.route.snapshot.paramMap.has('id');
+    this.categoryId = hasCategoryIdParam
+      ? +this.route.snapshot.paramMap.get('id')
+      : 1;
+
+    this.productService.getProductList(this.categoryId).subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
--- frontend/src/app/service/product.service.ts
+++ frontend/src/app/service/product.service.ts
@@ -19,15 +19,31 @@ export class ProductService {
   getProductList(categoryId: number): Observable<Product[]> {
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${categoryId}`;
 
-    return this.httpClient
-      .get<GetResponseProducts>(searchUrl)
-      .pipe(map((response) => response._embedded.products));
+    return this.getProducts(searchUrl);
   }
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient
-    .get<GetResponseProductCategory>(this.categoryUrl)
-    .pipe(map((response) => response._embedded.productCategory));
+      .get<GetResponseProductCategory>(this.categoryUrl)
+      .pipe(map((response) => response._embedded.productCategory));
+  }
+
+  searchProducts(keyword: string): Observable<Product[]> {
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${keyword}`;
+
+    return this.getProducts(searchUrl);
+  }
+
+  getProduct(productId: number): Observable<Product> {
+    const searchUrl = `${this.baseUrl}/${productId}`;
+
+    return this.httpClient.get<Product>(searchUrl);
+  }
+
+  private getProducts(searchUrl: string): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponseProducts>(searchUrl)
+      .pipe(map((response) => response._embedded.products));
   }
 }
 
